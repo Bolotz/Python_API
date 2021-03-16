77666cf4c5790fceb2323a232ce30a589b206c47
nromano263 <78044734+nromano263@users.noreply.github.com>
2021-03-16 12:36:12 -0500

    Add files via upload

---
+++ prime.py
@@ -0,0 +1,15 @@
+
+
+num = int(input("Provide a Number:"))
+
+if num > 1:
+
+    for i in range(2, int(num/2)+1):
+
+        if (num % i) == 0:
+            print(False)
+            break
+    else:
+        print(True)
+else:
+    print(False)
